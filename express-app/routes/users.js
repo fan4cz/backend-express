cedee3f2751d25553a7f4d51520b1796ce5bf325
fan4cz <fan4cz@gmail.com>
2026-03-24 19:29:58 +0500

    dsfv

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -1,42 +1,37 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const sqlite3 = require('sqlite3').verbose();   
+const sqlite3 = require("sqlite3").verbose();
 
-const db = new sqlite3.Database('mydb.db');
-
-const users = [
-  { id: 1, name: "Скворок Артем" },
-  { id: 2, name: "Мальгин Иван" }
-];
+const db = new sqlite3.Database("mydb.db");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get("/", function (req, res, next) {
   db.all("SELECT id, name FROM users", [], (err, rows) => {
     if (err) {
       console.log(err);
-    } else {    
+    } else {
       res.json({ items: rows });
     }
   });
 });
 
-router.get('/:id', function(req, res, next) {
+router.get("/:id", function (req, res, next) {
   db.all("SELECT id, name FROM users WHERE id = ?", [id], (err, rows) => {
     if (err) {
-        console.log(err);
-    } else {    
+      console.log(err);
+    } else {
       res.json({ items: rows });
-    }  
+    }
   });
-  
+
   res.json(user);
 });
 
-router.post('/', function(req, res, next) {  
-  const name = req.body.name
+router.post("/", function (req, res, next) {
+  const name = req.body.name;
   const insert = "INSERT INTO users (name) VALUES (?)";
 
-  db.run(insert, [name], function(err) {
+  db.run(insert, [name], function (err) {
     if (err) {
       console.log(err);
     }
