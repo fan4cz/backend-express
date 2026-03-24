577894bf57ebd50a88686fc37f168c15aa3fc705
fan4cz <fan4cz@gmail.com>
2026-03-24 19:27:14 +0500

    oeajklerklger;eragjiergeraergergaeyerayeraerhihgjerhgjlkdhfrghnbnkbnearo;bhatnkldnbnklgjnbodlfjhgndfoklnfg/l;fgmbfnkldz

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const sqlite3 = require('sqlite3').verbose();   
+
+const db = new sqlite3.Database('mydb.db');
 
 const users = [
   { id: 1, name: "Скворок Артем" },
@@ -8,27 +11,37 @@ const users = [
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.json({ items: users });
+  db.all("SELECT id, name FROM users", [], (err, rows) => {
+    if (err) {
+      console.log(err);
+    } else {    
+      res.json({ items: rows });
+    }
+  });
 });
 
 router.get('/:id', function(req, res, next) {
-  const userId = parseInt(req.params.id);
-  const user = users.find(u => u.id === userId);
-  
-  if (!user) {
-    return res.status(404).json({ error: 'User not found' });
-  }
+  db.all("SELECT id, name FROM users WHERE id = ?", [id], (err, rows) => {
+    if (err) {
+        console.log(err);
+    } else {    
+      res.json({ items: rows });
+    }  
+  });
   
   res.json(user);
 });
 
-router.post('/', function(req, res, next) {
-  const newUser = {
-    id: users.length + 1,
-    name: req.body.name
-  };
-  users.push(newUser);
-  res.status(201).json(newUser);
+router.post('/', function(req, res, next) {  
+  const name = req.body.name
+  const insert = "INSERT INTO users (name) VALUES (?)";
+
+  db.run(insert, [name], function(err) {
+    if (err) {
+      return res.status(500).json({ error: 'Database error' });
+    }
+    res.status(201).json({ id: this.lastID, name });
+  });
 });
 
 module.exports = router;
