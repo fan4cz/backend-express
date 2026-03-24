2bda37c9e3440eb1a21d011325aaa7c17807340a
fan4cz <fan4cz@gmail.com>
2026-03-24 18:51:35 +0500

    dfsrg

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -1,14 +1,24 @@
 const express = require('express');
 const router = express.Router();
 
+const users = [
+  { id: 1, name: "Скворок Артем" },
+  { id: 2, name: "Мальгин Иван" }
+];
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.json({
-    items: [
-      {id: 1, name: "Скворок Артем"},
-      {id: 2, name: "Мальгин Иван"}
-    ]
-  });
+  res.json({ items: users });
+});
+
+
+router.post('/', function(req, res, next) {
+  const newUser = {
+    id: users.length + 1,
+    name: req.body.name
+  };
+  users.push(newUser);
+  res.status(201).json(newUser);
 });
 
 module.exports = router;
