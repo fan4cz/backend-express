d170098648c3b5e47f4e3e70effe983bccd17204
fan4cz <fan4cz@gmail.com>
2026-03-24 19:48:00 +0500

    bsfg

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -1,10 +1,5 @@
 const express = require("express");
 const router = express.Router();
-<<<<<<< Updated upstream
-const sqlite3 = require("sqlite3").verbose();
-
-const db = new sqlite3.Database("mydb.db");
-=======
 
 const sqlite3 = require('sqlite3').verbose()
 const db = new sqlite3.Database('mydb.db');
@@ -16,7 +11,6 @@ const users = [
   { id: 1, name: "Скворок Артем" },
   { id: 2, name: "Мальгин Иван" }
 ];
->>>>>>> Stashed changes
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
@@ -29,26 +23,16 @@ router.get("/", function (req, res, next) {
   });
 });
 
-<<<<<<< Updated upstream
-router.get("/:id", function (req, res, next) {
-=======
 router.get('/:id', function(req, res, next) {
   const { id } = req.params;
 
->>>>>>> Stashed changes
   db.all("SELECT id, name FROM users WHERE id = ?", [id], (err, rows) => {
     if (err) {
       console.log(err);
     } else {
       res.json({ items: rows });
     }
-<<<<<<< Updated upstream
-  });
-
-  res.json(user);
-=======
   });  
->>>>>>> Stashed changes
 });
 
 router.post("/", function (req, res, next) {
