73cee5139d0130b4bb15089ef67bb3cc868b775b
fan4cz <fan4cz@gmail.com>
2026-03-24 18:41:22 +0500

    апвравр

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -3,7 +3,12 @@ const router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('Hello, world!');
+  res.json({
+    items: [
+      {id: 1, name: "Скворок Артем"},
+      {id: 2, name: "Мальгин Иван"}
+    ]
+  });
 });
 
 module.exports = router;
